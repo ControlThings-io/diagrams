b3a48ee301521c39ead24c3f2d097eafbfba4086
Justin Searle <justin@controlthings.io>
2021-12-01 16:01:48 -0700

    Updated build script and cleanup

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 DELETED=`ls img/ | wc -l`
 rm img/*
 cd src/
-plantuml -v -Tpng -Tsvg -o ../img/ * 2>&1 | grep "Creating file"
+plantuml -v -Tsvg -o ../img/ * 2>&1 | grep "Creating file"
 plantuml -v -Tpng -o ../img/ * 2>&1 | grep "Creating file"
 echo "Deleted $DELETED old files in ./img/"
 echo "Created `ls ../img/ | wc -l` new files in ./img/"
